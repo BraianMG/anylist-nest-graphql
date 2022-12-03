6c52dc6ab22ce67c2e06f1c58d9f80adbbeff6e7
Braian <braian.gonzales77@gmail.com>
2022-12-03 19:22:51 -0300

    crear custom enum ValidRoles y registrarlo en GraphQL

--- src/auth/enums/valid-roles.enum.ts
+++ src/auth/enums/valid-roles.enum.ts
@@ -1,6 +1,12 @@
-// TODO: Implementar enum como GraphQL Enum Type
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum ValidRoles {
   ADMIN = 'admin',
   USER = 'user',
   SUPER_USER = 'superUser',
 }
+
+registerEnumType(ValidRoles, {
+  name: 'ValidRoles',
+  description: 'Nisi anim pariatur elit cillum amet voluptate in veniam.',
+});
