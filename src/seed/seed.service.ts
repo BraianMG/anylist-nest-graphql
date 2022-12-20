7b1fd124a0fda05a4db80f9dac4afa1c324f4792
Braian <braian.gonzales77@gmail.com>
2022-12-20 12:13:55 -0300

    proteger seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
-  constructor() {}
+  private isProd: boolean;
+
+  constructor(private readonly configService: ConfigService) {
+    this.isProd = configService.get('STATE') === 'prod';
+  }
 
   async executeSeed(): Promise<boolean> {
-    
+    if (this.isProd)
+      throw new UnauthorizedException('We cannot run SEED on Prod');
+
     return true;
   }
 }
