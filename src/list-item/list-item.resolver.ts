fbf569f38ddc0fb6a49d10236c218f306e98d424
Braian <braian.gonzales77@gmail.com>
2022-12-20 19:38:03 -0300

    insertar ListItems

--- src/list-item/list-item.resolver.ts
+++ src/list-item/list-item.resolver.ts
@@ -1,17 +1,21 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ListItemService } from './list-item.service';
 import { ListItem } from './entities/list-item.entity';
 import { CreateListItemInput } from './dto/create-list-item.input';
 import { UpdateListItemInput } from './dto/update-list-item.input';
+import { UseGuards } from '@nestjs/common/decorators';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Resolver(() => ListItem)
+@UseGuards(JwtAuthGuard)
 export class ListItemResolver {
   constructor(private readonly listItemService: ListItemService) {}
 
   @Mutation(() => ListItem)
-  createListItem(
+  async createListItem(
     @Args('createListItemInput') createListItemInput: CreateListItemInput,
-  ) {
+    //! TODO: Podría pedir el usuario para validarlo
+  ): Promise<ListItem> {
     return this.listItemService.create(createListItemInput);
   }
 
@@ -20,23 +24,23 @@ export class ListItemResolver {
     return this.listItemService.findAll();
   }
 
-  @Query(() => ListItem, { name: 'listItem' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.listItemService.findOne(id);
-  }
+  // @Query(() => ListItem, { name: 'listItem' })
+  // findOne(@Args('id', { type: () => Int }) id: number) {
+  //   return this.listItemService.findOne(id);
+  // }
 
-  @Mutation(() => ListItem)
-  updateListItem(
-    @Args('updateListItemInput') updateListItemInput: UpdateListItemInput,
-  ) {
-    return this.listItemService.update(
-      updateListItemInput.id,
-      updateListItemInput,
-    );
-  }
+  // @Mutation(() => ListItem)
+  // updateListItem(
+  //   @Args('updateListItemInput') updateListItemInput: UpdateListItemInput,
+  // ) {
+  //   return this.listItemService.update(
+  //     updateListItemInput.id,
+  //     updateListItemInput,
+  //   );
+  // }
 
-  @Mutation(() => ListItem)
-  removeListItem(@Args('id', { type: () => Int }) id: number) {
-    return this.listItemService.remove(id);
-  }
+  // @Mutation(() => ListItem)
+  // removeListItem(@Args('id', { type: () => Int }) id: number) {
+  //   return this.listItemService.remove(id);
+  // }
 }
