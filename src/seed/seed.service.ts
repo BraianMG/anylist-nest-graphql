fbf45e2abab539e1b3e8162ba8759fd0ccfb9ca6
Braian <braian.gonzales77@gmail.com>
2022-12-20 12:14:34 -0300

    borrar registros anteriores

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/items/entities/item.entity';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {
     this.isProd = configService.get('STATE') === 'prod';
   }
 
@@ -13,6 +23,23 @@ export class SeedService {
     if (this.isProd)
       throw new UnauthorizedException('We cannot run SEED on Prod');
 
+    await this.deleteDatabase();
     return true;
+  }
+
+  async deleteDatabase() {
+    // Borrar Items
+    await this.itemsRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
+
+    // Borrar Users
+    await this.usersRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 }
