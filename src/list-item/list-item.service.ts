1a1aff3f7126c37643cfb59507ac66a1921eb25f
Braian <braian.gonzales77@gmail.com>
2023-01-12 18:57:41 -0300

    devolver información completa al crear un ListItem

--- src/list-item/list-item.service.ts
+++ src/list-item/list-item.service.ts
@@ -23,7 +23,9 @@ export class ListItemService {
       list: { id: listId },
     });
 
-    return this.listItemsRepository.save(newListItem);
+    await this.listItemsRepository.save(newListItem);
+
+    return this.findOne(newListItem.id);
   }
 
   async findAll(
